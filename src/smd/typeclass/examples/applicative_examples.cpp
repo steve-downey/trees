5b86667884ebe892d06c569b4330ce79e8b4dda9
Steve Downey <sdowney2@bloomberg.net>
2026-04-26 23:21:39 -0400

    cppnow26_trees_checkpoint

--- src/smd/typeclass/examples/applicative_examples.cpp
+++ src/smd/typeclass/examples/applicative_examples.cpp
@@ -1,15 +1,8 @@
-#include <smd/tree/fix_tree.hpp>
-#include <smd/tree/fix_tree_applicative.hpp>
-#include <smd/typeclass/applicative.hpp>
 
-using namespace smd::tree;
+#include <functional>
 
-int main(){
-  auto f = FixTree<std::function<int(int)>>::leaf(
-    [](int x){return x+1;});
-  auto x = FixTree<int>::node(
-    FixTree<int>::leaf(1),
-    FixTree<int>::leaf(2));
-
-  auto r = smd::map<smd::applicative_tag, decltype(x)>::apply(f,x);
+// 3f0c8d0e
+int main() {
+  return 0;
 }
+// 3f0c8d0e end
