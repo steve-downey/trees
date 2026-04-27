e2352384c13ef169f1beaa477c6fbb836b22b8f5
Steve Downey <sdowney2@bloomberg.net>
2026-04-26 23:22:51 -0400

    cppnow26_phase4_complete

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <functional>
+#include <smd/tree/fix_tree.hpp>
+#include <smd/tree/fix_tree_applicative.hpp>
+#include <smd/typeclass/applicative.hpp>
+using namespace smd::tree;
+int main(){
+ auto f=FixTree<std::function<int(int)>>::leaf([](int x){return x+1;});
+ auto x=FixTree<int>::node(FixTree<int>::leaf(1),FixTree<int>::leaf(2));
+ auto r=smd::invoke<FixTree<int>>(f,x);
+}
