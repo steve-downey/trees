2164e1f06d75b08159a4c73a2990a2748e4b87ae
Steve Downey <sdowney2@bloomberg.net>
2026-04-29 00:58:12 -0400

    binary tree

---
+++ src/smd/tree/binary_tree.hpp
@@ -0,0 +1,82 @@
+#ifndef INCLUDE_SMD_TREE_BINARY_TREE_HPP
+#define INCLUDE_SMD_TREE_BINARY_TREE_HPP
+
+#include <cassert>
+#include <memory>
+#include <utility>
+
+namespace smd::tree {
+
+template <class T>
+class BinaryTree {
+  T d_value;
+  std::shared_ptr<BinaryTree> d_left;
+  std::shared_ptr<BinaryTree> d_right;
+
+ public:
+  using value_type = T;
+
+  static auto leaf(T value) -> BinaryTree
+  {
+    return BinaryTree(std::move(value), {}, {});
+  }
+
+  static auto node(T value, BinaryTree left, BinaryTree right) -> BinaryTree
+  {
+    return BinaryTree(std::move(value),
+                      std::make_shared<BinaryTree>(std::move(left)),
+                      std::make_shared<BinaryTree>(std::move(right)));
+  }
+
+  static auto branch(T value, BinaryTree left, BinaryTree right) -> BinaryTree
+  {
+    return node(std::move(value), std::move(left), std::move(right));
+  }
+
+  static auto from_children_ptrs(T value,
+                                 std::shared_ptr<BinaryTree> left,
+                                 std::shared_ptr<BinaryTree> right)
+    -> BinaryTree
+  {
+    return BinaryTree(std::move(value), std::move(left), std::move(right));
+  }
+
+  static auto make_ptr(BinaryTree tree) -> std::shared_ptr<BinaryTree>
+  {
+    return std::make_shared<BinaryTree>(std::move(tree));
+  }
+
+  auto value() const -> const T& { return d_value; }
+
+  auto has_left() const -> bool { return static_cast<bool>(d_left); }
+  auto has_right() const -> bool { return static_cast<bool>(d_right); }
+
+  auto left() const -> const BinaryTree&
+  {
+    assert(d_left);
+    return *d_left;
+  }
+
+  auto right() const -> const BinaryTree&
+  {
+    assert(d_right);
+    return *d_right;
+  }
+
+  auto left_ptr() const -> const std::shared_ptr<BinaryTree>& { return d_left; }
+  auto right_ptr() const -> const std::shared_ptr<BinaryTree>& { return d_right; }
+
+ private:
+  BinaryTree(T value,
+             std::shared_ptr<BinaryTree> left,
+             std::shared_ptr<BinaryTree> right)
+      : d_value(std::move(value))
+      , d_left(std::move(left))
+      , d_right(std::move(right))
+  {
+  }
+};
+
+}  // close namespace smd::tree
+
+#endif
